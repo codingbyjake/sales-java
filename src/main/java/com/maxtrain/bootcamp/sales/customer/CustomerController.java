b19f9381c8568a1ce49e4673eb5011fb994439ba
Jacob Witte <wittejg@gmail.com>
2023-03-22 16:29:31 -0400

    first commit

---
+++ src/main/java/com/maxtrain/bootcamp/sales/customer/CustomerController.java
@@ -0,0 +1,43 @@
+package com.maxtrain.bootcamp.sales.customer;
+
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/customers")
+public class CustomerController { // start of class
+	
+	@Autowired
+	private CustomerRepository custRepo;
+
+	@GetMapping
+	public ResponseEntity<Iterable<Customer>> getCustomers() {
+		Iterable<Customer> customers = custRepo.findAll();
+		return new ResponseEntity<Iterable<Customer>>(customers, HttpStatus.OK);
+	}
+	
+	@GetMapping("{id}")
+	public ResponseEntity<Customer> getCustomer(@PathVariable int id){
+		Optional<Customer> customer = custRepo.findById(id);
+		if(customer.isEmpty()) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<Customer>(customer.get(), HttpStatus.OK);
+	}
+
+	@GetMapping("code/{code}")
+	public ResponseEntity<Customer> getCustomerByCode(@PathVariable String code){
+		Optional<Customer> customer = custRepo.findByCode(code);
+		if(customer.isEmpty()) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<Customer>(customer.get(), HttpStatus.OK);
+	}
+} //end of class
